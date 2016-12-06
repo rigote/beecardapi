d27ae88e74be40556dc3bb5ed1b580c9166b8497
Ricardo Ghion Fermino <Ricardo Ghion Fermino>
2016-12-06 00:35:00 -0200

    Criada a estrutura básica da solution da API

---
+++ BeeCard/BeeCard.API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BeeCard.API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ BeeCard/BeeCard.Infrastructure/Context.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+
+namespace BeeCard.Infrastructure
+{
+    public class Context : DbContext
+    {
+        public Context()
+            : base()
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
