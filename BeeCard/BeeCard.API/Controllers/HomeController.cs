e329294d6dd8605e89996c5bcd8af3f39cd61563
ricar <ricar@DESKTOP-SI3Q26H>
2016-12-16 23:29:41 -0200

    Alterações nos arquivos de configuração

--- BeeCard/BeeCard.API/Controllers/HomeController.cs
+++ BeeCard/BeeCard.API/Controllers/HomeController.cs
@@ -1,16 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using BeeCard.Application.Interfaces;
 using System.Web.Mvc;
 
 namespace BeeCard.API.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ITestAppService _testService;
+        public HomeController(ITestAppService testService)
+        {
+            _testService = testService;
+        }
+
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
+
+            _testService.DoTest();
 
             return View();
         }
