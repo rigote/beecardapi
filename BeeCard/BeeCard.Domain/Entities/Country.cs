39b59f236c610c083e719a5944ef593d56550443
diegolavas <diegolavas@gmail.com>
2017-06-24 19:10:19 -0300

    retornei a propriedade companies dentro do country. Tenho duvidas.

--- BeeCard/BeeCard.Domain/Entities/Country.cs
+++ BeeCard/BeeCard.Domain/Entities/Country.cs
@@ -1,12 +1,16 @@
-﻿namespace BeeCard.Domain.Entities
+﻿using System.Collections.Generic;
+
+namespace BeeCard.Domain.Entities
 {
     public class Country : BaseEntity
     {
         public string Name { get; set; }
+        public virtual ICollection<Company> Companies { get; set; }
 
         public Country()
         {
             Name = string.Empty;
+            Companies = new List<Company>();
         }
     }
 }
